4e623a7c638b564928ff050fa890be7a9259dd4b
Luis Mendes <machadoluismendes@gmail.com>
2022-01-21 12:56:51 -0300

    svelte increment
    Auth0

---
+++ examples/svelte/src/auth_config.js
@@ -0,0 +1,10 @@
+// https://auth0.com/blog/authenticating-svelte-apps/
+
+// auth_config.js
+
+const config = {
+    domain: "YOUR_AUTH0_DOMAIN",
+    clientId: "YOUR_APP_CLIENT_ID"
+  };
+  
+  export default config;
